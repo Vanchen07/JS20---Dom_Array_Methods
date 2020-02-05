0976410a77a2ad6aa67189ec6040e46d31145bc8
Vanessa Chen <vanchen07@gmail.com>
2020-02-05 14:19:03 -0800

    implement calculate wealth feature

--- script.js
+++ script.js
@@ -49,6 +49,16 @@ function showMillionaires() {
     updateDom();
 }
 
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+
+    main.appendChild(wealthEl);
+}
+
 function updateDom(providedData = data) {
     main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
     providedData.forEach((item) => {
@@ -66,4 +76,5 @@ function formatMoney(num) {
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
-showMillionairesBtn.addEventListener('click', showMillionaires)
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
